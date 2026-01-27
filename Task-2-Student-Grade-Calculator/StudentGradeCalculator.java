cfb502039ca5d801d9fa52395a68eb145e61c8fe
vaishnavipatil1107 <patilvaishnavirajendra@gmail.com>
2026-01-27 19:48:23 +0530

    Implement Student Grade Calculator functionality

---
+++ Task-2-Student-Grade-Calculator/StudentGradeCalculator.java
@@ -0,0 +1,44 @@
+package com.task2;
+
+import java.util.Scanner;
+
+public class studentgradecalculator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of subjects: ");
+        int subjects = sc.nextInt();
+
+        int totalMarks = 0;
+
+        for (int i = 1; i <= subjects; i++) {
+            System.out.print("Enter marks for subject " + i + " (out of 100): ");
+            int marks = sc.nextInt();
+            totalMarks += marks;
+        }
+
+        double averagePercentage = (double) totalMarks / subjects;
+        char grade;
+
+        if (averagePercentage >= 90) {
+            grade = 'A';
+        } else if (averagePercentage >= 75) {
+            grade = 'B';
+        } else if (averagePercentage >= 60) {
+            grade = 'C';
+        } else if (averagePercentage >= 40) {
+            grade = 'D';
+        } else {
+            grade = 'F';
+        }
+
+        System.out.println("\n--- Result ---");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+
+        sc.close();
+    }
+}
